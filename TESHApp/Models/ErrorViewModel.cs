5979ec7345f4880822e751468c116c1c4fb63562
Araceli Carbajal <cbjlara@outlook.com>
2019-09-11 16:09:21 -0500

    Add project files.

---
+++ TESHApp/Models/ErrorViewModel.cs
@@ -0,0 +1,27 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyModel;
+
+
+namespace TESHApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+    public class Alumnos
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        [Key]
+        public int Id { get; set; }
+
+
+    }
+}
